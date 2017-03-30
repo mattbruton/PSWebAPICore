8431478a588e07497c0474ee81159ed43d231a42
MATT BRUTON <the.bruton@gmail.com>
2017-03-29 22:23:28 -0500

    created city model, cities data store, city controller that returns list of cities or individual city

---
+++ VSMacAPI.API/Controllers/CitiesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VSMacAPI.API.Controllers
+{	
+	[Route("api/cities/")]
+	public class CitiesController : Controller
+	{
+		[HttpGet()]
+		public JsonResult GetCities()
+		{
+			return new JsonResult(CitiesDataStore.Current.Cities);
+		}
+
+	    [HttpGet("{id}")]
+	    public JsonResult GetCity(int id)
+	    {
+	        return new JsonResult(
+	            CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == id)
+	        );
+	    }
+	}
+}
